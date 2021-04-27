0fb66621f5587a702f2de9f6e18ccfbdd83de22a
TAKTAK Hajer <taktakhajer@gmail.com>
2021-04-27 10:04:47 +0200

    Ajoutez des fichiers projet.

---
+++ MyFirstWebAPP-DMWM/Controllers/CategoryController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MyFirstWebAPP_DMWM.Data;
+
+namespace MyFirstWebAPP_DMWM.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        //action result pour lister les catégories
+        //HTTP-GET
+        public IActionResult Index()
+        {
+            var cat = _db.Categories.ToList();
+            return View(cat);
+        }
+    }
+}
---
+++ MyFirstWebAPP-DMWM/Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using MyFirstWebAPP_DMWM.Models;
+
+namespace MyFirstWebAPP_DMWM.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<SubCategory> Subcategories { get; set; }
+    }
+}
